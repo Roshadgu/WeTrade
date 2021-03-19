c6755ddc8b5c784a4cf8b7df6f3dc032552dc389
Roshadgu <roshadgu@gmail.com>
2021-03-19 17:52:48 -0400

    Finished the design of the welcome screen and the log in screen

--- app/src/main/java/com/example/androiddevchallenge/LogInActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/LogInActivity.kt
@@ -1,25 +1,39 @@
 package com.example.androiddevchallenge
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.typography
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+
 class LogInActivity : AppCompatActivity()
 {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,11 +44,73 @@ class LogInActivity : AppCompatActivity()
   }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFF322049, heightDp = 640, widthDp = 360)
+@Composable
+fun loginUI()
+{
+ // Icon(Icons.Filled., contentDescription = null)
+  val negativeXoffest = 134.dp
+
+  //Password Material Icon
+  Image(
+    painter = painterResource(id = R.drawable.password_black_18dp),
+    contentDescription = null,
+    Modifier
+      .height(30.dp)
+      .width(30.dp)
+      .offset(x = -negativeXoffest, y = 127.dp),
+    contentScale = ContentScale.Inside
+  )
+
+  //Mail Material Icon
+  Image(
+    painter = painterResource(id = R.drawable.mail_outline_24px),
+    contentDescription = null,
+    Modifier
+      .height(30.dp)
+      .width(30.dp)
+      .offset(x = -negativeXoffest, y = 56.dp),
+    contentScale = ContentScale.Inside
+  )
+
+  Box(modifier = Modifier.fillMaxHeight())
+  {
+    Image(
+      painter = painterResource(id = R.drawable.login_bg),
+      contentDescription = null,
+      modifier = Modifier.fillMaxWidth(),
+      contentScale = ContentScale.FillWidth
+    )
+  }
+}
+
+@Composable
+fun loginButton()
+{
+  val yellow = Color(0xFFE3DA00)
+
+  Button(
+    onClick ={/*Toast.makeText(applicationContext, "You've Logged In!", Toast.LENGTH_LONG).show()*/},
+    modifier = Modifier
+      .offset(y = 100.dp)
+      .width(350.dp)
+      .height(48.dp)
+      .clip(shape = RoundedCornerShape(20.dp)),
+    colors = ButtonDefaults.buttonColors(backgroundColor = yellow)
+  )
+  {
+    Text(text = "LOG IN")
+  }
+}
+
 @Composable
 fun LoginScreen()
 {
-  Row(verticalAlignment = Alignment.CenterVertically) {
+  var value = ""
+  var value2 = ""
+  val offsetValue = 100.dp
+  var ph = "Email address"
+
+  Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxHeight()) {
     Column(
       horizontalAlignment = Alignment.CenterHorizontally,
       modifier = Modifier
@@ -42,39 +118,57 @@ fun LoginScreen()
         .fillMaxWidth()
         .padding(20.dp))
     {
-      Text( "Welcome bbbbbback",
-        style = typography.h1,
-        fontFamily = montserratFamily,
+      Text( "Welcome back",
+        style = typography.h2,
+        //fontFamily = montserratFamily,
         fontWeight = FontWeight.ExtraBold,
         color = Color.White,
-        fontSize = 96.sp
+        fontSize = 50.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.offset(y = -offsetValue)
       )
-      //TextField(value = "email address", onValueChange = { /*TODO*/ })
+      //Email Address Text Field
+      OutlinedTextField(
+        value = value,
+        label = {Text("Email address",  modifier = Modifier.offset(x = 30.dp))},
+        onValueChange = { value = it },
+        maxLines = 1,
+        textStyle = TextStyle(color = Color.Gray, fontWeight = FontWeight.Light),
+        modifier = Modifier
+          .width(350.dp)
+          .height(60.dp)
+          .offset(y = 40.dp)
+          .clip(shape = RoundedCornerShape(4.dp)),
+        colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = Color.Black, unfocusedBorderColor = Color.Gray)
+      )
+      //Password Text Field
+      OutlinedTextField(
+        value = value2,
+        placeholder = {"Password"},
+        onValueChange = { value2 = it },
+        label = { Text("Password", modifier = Modifier.offset(x = 30.dp))},
+        maxLines = 1,
+        textStyle = TextStyle(color = Color.Gray, fontWeight = FontWeight.Light),
+        modifier = Modifier
+          .width(350.dp)
+          .height(60.dp)
+          .offset(y = 48.dp)
+          .clip(shape = RoundedCornerShape(4.dp))
+      )
+
+      loginButton()
     }
   }
 }
-/*
-@Preview(showBackground = true, backgroundColor = 0xFF322049, heightDp = 640, widthDp = 360)
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF, heightDp = 640, widthDp = 360)
 @Composable
-fun PreviewThis()
+fun LoginScreenPreview()
 {
+  loginUI()
   LoginScreen()
 }
-*/
-
 /*
-* @Preview(
-    widthDp = 420
-)
-@Composable
-fun SpotifyCardPreview() {
-    SpotifyCard()
-}
-
-@Composable
-fun SpotifyCard(color: Color = Color(0xFF8398DB)) {}
-* */
-
 val montserratFamily = FontFamily(
   Font(R.font.montserrat_black, FontWeight.Black),
   Font(R.font.montserrat_black_italic, FontWeight.Black),
@@ -84,3 +178,4 @@ val montserratFamily = FontFamily(
   Font(R.font.montserrat_medium, FontWeight.Medium),
   Font(R.font.montserrat_light, FontWeight.Light)
 )
+ */
--- app/src/main/java/com/example/androiddevchallenge/WelcomeActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/WelcomeActivity.kt
@@ -3,14 +3,105 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.shapes
 
-
-class WelcomeActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
+class WelcomeActivity : AppCompatActivity()
+{
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
     super.onCreate(savedInstanceState)
     setContent {
-
+      welcomeScreen()
     }
   }
+}
+
+@Composable
+fun logInButon()
+{
+  val yellow = Color(0xFFE3DA00)
+  //val clear = Color(transparent)
+
+  OutlinedButton(
+    onClick ={/*Toast.makeText(applicationContext, "Time to sign up!", Toast.LENGTH_LONG).show()*/},
+    modifier = Modifier
+      .offset(x = 200.dp, y = 550.dp)
+      .width(150.dp)
+      .height(48.dp),
+    border = BorderStroke(2.dp, yellow),
+    shape = RoundedCornerShape(50),
+    colors = ButtonDefaults.outlinedButtonColors(Color.Transparent)
+  )
+  {
+    Text(text = "LOG IN", color = yellow,)
+  }
+}
+
+@Composable
+fun signUpButton()
+{
+  val yellow = Color(0xFFE3DA00)
+
+  Button(
+    onClick ={/*Toast.makeText(applicationContext, "Time to sign up!", Toast.LENGTH_LONG).show()*/},
+    modifier = Modifier
+      .offset(x = 20.dp, y = 550.dp)
+      .width(150.dp)
+      .height(48.dp),
+    colors = ButtonDefaults.buttonColors(backgroundColor = yellow),
+    shape = RoundedCornerShape(50)
+  )
+  {
+    Text(text = "GET STARTED")
+  }
+}
+
+@Composable
+fun welcomeScreen()
+{
+  Box(modifier = Modifier.fillMaxHeight())
+  {
+    Image(
+      painter = painterResource(id = R.drawable.welcome_bg),
+      contentDescription = null,
+      modifier = Modifier.fillMaxWidth(),
+      contentScale = ContentScale.FillWidth
+    )
+
+    Image(
+      painter = painterResource(id = R.drawable.logo),
+      contentDescription = null,
+      modifier = Modifier.offset(x = 100.dp, y = 300.dp)
+    )
+
+    signUpButton()
+    logInButon()
+  }
+}
+
+@Preview(heightDp = 640, widthDp = 360)
+@Composable
+fun PreviewWelcomeScreen()
+{
+  welcomeScreen()
 }
